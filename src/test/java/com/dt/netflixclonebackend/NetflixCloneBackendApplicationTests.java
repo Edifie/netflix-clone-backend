f102b5fe161788f0ca5895092909a039796a9e06
Dilan Taskin <dilantaskin2@hotmail.com>
2024-03-16 15:19:32 +0300

    first commit

---
+++ src/test/java/com/dt/netflixclonebackend/NetflixCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.netflixclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
